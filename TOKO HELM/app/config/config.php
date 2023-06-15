4f3f9372fd175b7ded28e1a3bf489663b8571cdb
Ratih <113415841+Ratihmardianingsih@users.noreply.github.com>
2023-06-15 07:27:29 -0700

    Add files via upload

---
+++ TOKO HELM/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/pencatatan/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pencatatan_Tokohelm');
